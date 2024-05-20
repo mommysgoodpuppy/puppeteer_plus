83fbdf217f088563921f86e4794450b17d193b28
mommysgoodpuppy <theelectronicfreezer@gmail.com>
2024-05-20 14:47:02 +0300

    try update puppeteer to 22.9.0

--- core.ts
+++ core.ts
@@ -1,4 +1,4 @@
 import "./src/patch.ts";
 
-export * from "npm:puppeteer-core@22.7.1";
-export { default } from "npm:puppeteer-core@22.7.1";
+export * from "npm:puppeteer-core@22.9.0";
+export { default } from "npm:puppeteer-core@22.9.0";
--- src/install.ts
+++ src/install.ts
@@ -1,3 +1,3 @@
-import { downloadBrowser } from "npm:puppeteer@22.7.1/internal/node/install.js";
+import { downloadBrowser } from "npm:puppeteer@22.9.0/internal/node/install.js";
 
 await downloadBrowser();
